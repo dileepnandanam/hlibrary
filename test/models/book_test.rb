b5d53c6dfb20f2b2c0714314590e97b6bcf7439a
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-11-14 09:50:37 +0530

    adding model tests

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should return result on keyword search' do
+    book = Book.create(name: 'name123', author: 'author456', serial_number: 'a123')
+    assert Book.search('123', users('normal_user')).include?(book)
+  end
+
+  test 'should return books left to borrow' do
+    users('normal_user').borrow(books('one'))
+    assert Book.left_for(users('normal_user')).include?(books('two'))
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'user should borrow book' do
+    users('normal_user').borrow(books('one'))
+    assert users('normal_user').books.count == 1
+  end
+
+  test 'user should return book' do
+    users('normal_user').borrow(books('one'))
+    assert users('normal_user').books.count == 1
+
+    users('normal_user').return(books('one'))
+    assert users('normal_user').books.count == 0
+  end
 end
